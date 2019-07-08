8c6918fab8c94fb5233e5bc2728743690ae4a6ef
lixiaobing <li.xiaobing@phanta.cc>
2019-07-08 14:08:50 +0800

    add excel2Lua

---
+++ excel2lua/excel2lua.bat
@@ -0,0 +1,2 @@
+python excel2lua.py  test.xlsx test.lua
+pause
---
+++ excel2lua/test.lua
@@ -0,0 +1,66 @@
+--[[
+
+        test.lua
+        exported by excel2lua.py
+        from file:test.xlsx
+
+--]]
+
+test = {
+  [10001] = {
+   id = 10001,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'aaaa',
+   type = 1,
+   level = 10,
+  },
+  [10002] = {
+   id = 10002,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'bbbb',
+   type = 2,
+   level = 11,
+  },
+  [10003] = {
+   id = 10003,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'cccc',
+   type = 3,
+   level = 12,
+  },
+  [10004] = {
+   id = 10004,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'dddd',
+   type = 4,
+   level = 13,
+  },
+  [10005] = {
+   id = 10005,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'eeee',
+   type = 5,
+   level = 14,
+  },
+  [10006] = {
+   id = 10006,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'ffff',
+   type = 6,
+   level = 15,
+  },
+  [10007] = {
+   id = 10007,
+   name = '阿斯蒂芬',
+   note = '策划使用',
+   desc = 'gggg',
+   type = 7,
+   level = 16,
+  },
+}
